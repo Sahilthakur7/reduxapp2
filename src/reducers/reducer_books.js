55f7193be792a40080abefb9ba211c825ff49fd7
SahilThakur7 <sahilrd07@gmail.com>
2017-10-13 11:07:48 +0530

    added the reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title: 'JS:The good parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Alchemist'},
+        {title: 'The Monk Who Sold His Ferrari'}
+    ]
+}
